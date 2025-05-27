32a89e0c978dfd5b418c23334276cd67ebc42fe0
Nohemy254 <nohemymamani67@gmail.com>
2025-05-27 09:43:28 -0400

    Delete elecciones-electronicas directory

--- elecciones-electronicas/src/main/java/com/eleccioneselectronicas/repository/PersonaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eleccioneselectronicas.repository;
-
-import com.eleccioneselectronicas.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-    List<Persona> findByNombresContainingIgnoreCase(String nombre);
-}
