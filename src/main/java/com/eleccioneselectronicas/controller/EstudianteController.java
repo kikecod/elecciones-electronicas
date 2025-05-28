0717a08348f1aa3840f57bf3c51a9b6a3a153846
RakzoTV <136276357+RakzoTV@users.noreply.github.com>
2025-05-28 11:31:21 -0400

    Corregido el estudiante

--- src/main/java/com/eleccioneselectronicas/controller/EstudianteController.java
+++ src/main/java/com/eleccioneselectronicas/controller/EstudianteController.java
@@ -5,18 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.eleccioneselectronicas.dto.EstudianteDTO;
 import com.eleccioneselectronicas.service.EstudianteService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 
